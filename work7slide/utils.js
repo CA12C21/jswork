1ad6e56ce4bc262986d00fbbf0db9e81bedb9402
CA12C21 <1462718673@qq.com>
2019-10-24 00:24:33 +0800

    work7slide

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -1,8 +1,8 @@
 function bubbleSort(str) {
     let arr = str.split(',')
     let sortLog = []
-    for (let i =0; i <arr.length -1; i++) {
-    for (let j = 0; j <arr.length -i -1; j++) {
+    for (let i = 0; i <arr.length -1; i++) {
+    for (let j = 0; j <arr.length -i - 1; j++) {
         sortLog.push([arr.concat(), [j, j + 1]])
         sortLog.push([arr.concat(), [j, j + 1]])
         sortLog.push([arr.concat(), [j, j + 1]])
@@ -27,7 +27,7 @@ function showLog(logValues, showElement) {
 
     for (const key in row) {
         let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
-        str += '<span style="font-size:' + row[key] *20 +'px;' +
+        str += '<span style="font-size:' + row[key] * 20 + 'px;' +
                 color + '">' + row[key] + '</span>'
     }
 
